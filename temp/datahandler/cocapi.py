de1012b6d3d1afeaf4d65d4923327416bb795091
KioskaA <clashkioska@gmail.com>
2026-03-07 20:25:07 +0300

    Changes to be committed: 	modified:   .gitignore 	deleted:    cocapi/cocapi.py 	deleted:    cocapi/parser.py 	deleted:    descriptions/ai.py 	new file:   descriptions/broken.txt 	new file:   descriptions/desc.txt 	deleted:    descriptions/desc2.txt 	deleted:    descriptions/descmain.txt 	deleted:    descriptions/old.py 	deleted:    "descriptions/\320\232\320\275\320\270\320\263\320\2601.xlsx" 	deleted:    main.py 	modified:   temp/datahandler/cocapi.py 	modified:   temp/documentation/description.txt 	new file:   temp/storage/cw.txt 	new file:   temp/storage/cwl.txt 	new file:   temp/storage/players.txt 	new file:   temp/storage/raid.txt 	modified:   temp/storage/storage_creator.py 	deleted:    ui/extra_info.py 	deleted:    ui/members_table.py 	deleted:    ui/player_frame.py 	deleted:    ui/style.css 	deleted:    ui/ui.py

--- temp/datahandler/cocapi.py
+++ temp/datahandler/cocapi.py
@@ -6,11 +6,6 @@ APILogin = settings.coc_api["login"]
 APIPassword = settings.coc_api["password"]
 Clantag = settings.coc_api["clantag"]
 
-Wartag = None
-Playertag = "#R2CLJL8YR"
-Playertags = None
-AuthToken = None
-
 class DataGetter():
     def __init__(self):
         self.client = None
@@ -48,7 +43,7 @@ class DataGetter():
                 "offensive_reward": log.offensive_reward,
                 "defensive_reward": log.defensive_reward,
                 "total_loot": log.total_loot,
-                "completed_raid_count": log.total_loot,
+                "completed_raid_count": log.completed_raid_count,
                 "members": self.parse_raid_members(log.members)
             })
         return raidlog[0]
@@ -97,9 +92,38 @@ class DataGetter():
                 "builder_base_trophies": player.builder_base_trophies,
                 "town_hall": player.town_hall,
                 "donations": player.donations,
-                "received": player.received
+                "received": player.received,
+                "builder_hall": player.builder_hall,
+                "war_opted_in": player.war_opted_in,
+                "achievements": self.parse_achievement(player.achievements),
+                "builder_troops": self.parse_troops(player.builder_troops),
+                "equipment": self.parse_troops(player.equipment),
+                "heroes": self.parse_troops(player.heroes),
+                "home_troops": self.parse_troops(player.home_troops),
+                "pets": self.parse_troops(player.pets),
+                "spells": self.parse_troops(player.spells),
             })
         return players
+    
+    def parse_achievement(self, achievements):
+        result = []
+        for achievement in achievements:
+            result.append({
+                "name": achievement.name,
+                "stars": achievement.stars,
+                "value": achievement.value,
+                "target": achievement.target
+            })
+        return result
+
+    def parse_troops(self, troops):
+        result = []
+        for troop in troops:
+            result.append({
+                "name": troop.name,
+                "level": troop.level,
+            })
+        return result
     
     def parse_raid_members(self, memberslist):
         members = []
@@ -118,38 +142,291 @@ class DataGetter():
         war = await self.client.get_current_war(clantag)
         if not war.is_cwl:
             return "GOT CW INSTEAD OF CWL"
-        cwl = {
-            #"clan": war.clan,
-            #"opponent": war.opponent,
-            #"state": war.state.in_game_name,
-            #"end_time": str(war.end_time.time),
-            #"team_size": war.team_size,
-            "league_group": await self.parse_league_group(war.league_group, clantag),
-            #"attacks": war.attacks,
-            #"members": war.members,
-        }
+        cwl = await self.parse_league_group(war.league_group, clantag, war.team_size)
         return cwl
     
-    async def parse_league_group(self, league_group, clantag):
+    async def parse_league_group(self, league_group, clantag, team_size):
+        clans = league_group.clans
+        for clan in clans:
+            if clan.tag == clantag:
+                home_clan = clan
+                break
+        home_clan = self.parse_league_clan(home_clan)
+        clans = self.parse_league_clans(clans)
         wars = []
         async for war in league_group.get_wars_for_clan(clantag):
-            wars.append(war)
+            wars.append(self.parse_league_war(war))
         group = {
+            "team_size": team_size,
+            "state": league_group.state,
             "season": league_group.season,
             "number_of_rounds": league_group.number_of_rounds,
-            "clans": league_group.clans,
-            "home_clan_wars": wars
+            "clans": clans,
+            "home_clan": home_clan,
+            "home_clan_wars": wars,
+            "overlooked_players": self.get_overlooked_players(wars, [m["tag"] for m in home_clan["members"]])
         }
         return group
+    
+    def get_overlooked_players(self, wars, members):
+        completed_wars = [war for war in wars if war.get('state') == 'War Ended']
+        num_rounds = len(completed_wars)
 
-    async def getdata(self):
-        await self.client.get_clan(Clantag)
-        await self.client.get_clan_war(Clantag)
-        await self.client.get_league_group(Clantag)
-        await self.client.get_members(Clantag)
-        await self.client.get_player(Playertag)
-        await self.client.verify_player_token(Playertag, AuthToken)
+        if num_rounds == 0:
+            return {}
 
+        result = {i: [] for i in range(1, num_rounds + 1)}
+
+        for player in members:
+            missed = 0
+            for war in completed_wars:
+                attacked = war.get('attacked_players', [])
+                if player not in attacked:
+                    missed += 1
+
+            if 1 <= missed <= num_rounds:
+                result[missed].append(player)
+
+        return result
+    
+    def parse_league_war(self, war):
+        attacks = self.parse_league_attacks(war.attacks, war.clan.members)
+        league_war = {
+            "opponent": (war.opponent.tag, war.opponent.name),
+            "state": str(war.state),
+            "end_time": str(war.end_time.time),
+            "war_tag": war.war_tag,
+            "attacks": attacks,
+            "status": war.status,
+            "attacked_players": self.league_attacker_tags(attacks)
+        }
+        return league_war
+
+    def league_attacker_tags(self, attacks):
+        tags = []
+        for attack in attacks:
+            tags.append(attack["attacker_tag"])
+        return tags
+
+    def check_is_home_league_attack(self, attack, members):
+        attacker_tag = attack.attacker_tag
+        members = self.parse_league_members(members)
+        attacker_tags = []
+        for member in members:
+            attacker_tags.append(member["tag"])
+        if attacker_tag in attacker_tags:
+            return True
+        else:
+            return False
+
+    def parse_league_attacks(self, attacks, war):
+        league_attacks = []
+        for attack in attacks:
+            if self.check_is_home_league_attack(attack, war):
+                league_attacks.append(self.parse_league_attack(attack))
+        return league_attacks
+
+    def parse_league_attack(self, attack):
+        league_attack = {
+            "stars": attack.stars,
+            "destruction": attack.destruction,
+            "attacker_tag": attack.attacker_tag,
+            "defender_tag": attack.defender_tag,
+            "duration": attack.duration
+        }
+        return league_attack
+    
+    def parse_league_clan(self, clan):
+        league_clan = {
+            "tag": clan.tag,
+            "name": clan.name,
+            "members": self.parse_league_members(clan.members)
+        }
+        return league_clan
+    
+    def parse_league_clans(self, clans):
+        league_clans = []
+        for clan in clans:
+            league_clans.append(self.parse_league_clan(clan))
+        return league_clans
+    
+    def parse_league_members(self, members):
+        league_members = []
+        for member in members:
+            league_members.append({
+                "tag": member.tag,
+                "name": member.name,
+                "town_hall": member.town_hall
+            })
+        return league_members
+    
+    async def get_cw(self, clantag):
+        war = await self.client.get_current_war(clantag)
+        if war == None:
+            return "NO WAR"
+        if war.is_cwl:
+            return "GOT CWL INSTEAD OF CW"
+        home_members = self.parse_home_members(war.clan, war.members)
+        home_attacks = self.verify_attacks(home_members, war.attacks)
+        attack_states = self.create_cw_attackers_list(home_members)
+        cw = {
+            "home_clan": war.clan,
+            "opponent": war.opponent,
+            "state": str(war.state),
+            "end_time": str(war.end_time.time),
+            "team_size": war.team_size,
+            "status": str(war.status),
+            "home_members": home_members,
+            "attacks": home_attacks,
+            "ovelooked_members": attack_states[0],
+            "attacked_members": attack_states[1]
+        }
+        return cw
+    
+    def parse_cw_attacks(self, attacks):
+        cw_attacks = []
+        for attack in attacks:
+            cw_attacks.append({
+                "stars": attack.stars,
+                "destruction": attack.destruction,
+                "attacker_tag": attack.attacker_tag,
+                "duration": attack.duration,
+            })
+        return cw_attacks
+
+    def check_for_cw_attack(self, member):
+        if isinstance(member["attacks"], list) and len(member["attacks"]) > 0:
+            return True, member["tag"]
+        else:
+            return False, member["tag"]
+    
+    def create_cw_attackers_list(self, members):
+        ovelooked_members = []
+        attacked_members = []
+        for member in members:
+            res = self.check_for_cw_attack(member)
+            if res[0] == True:
+                attacked_members.append(res[1])
+            else:
+                ovelooked_members.append(res[1])
+        return ovelooked_members, attacked_members
+    
+    def verify_members(self, clan, members):
+        home_members = []
+        for member in members:
+            if member.clan == clan:
+                home_members.append(member)
+        return home_members
+    
+    def verify_attacks(self, home_members, attacks):
+        home_members_tags = []
+        for member in home_members:
+            home_members_tags.append(member["tag"])
+        home_attacks = []
+        for attack in attacks:
+            if attack.attacker_tag in home_members_tags:
+                home_attacks.append(attack)
+        return self.parse_cw_attacks(home_attacks)
+
+    def parse_home_members(self, clan, members):
+        home_members = []
+        for member in self.verify_members(clan, members):
+            home_members.append({
+                "tag": member.tag,
+                "name": member.name,
+                "town_hall": member.town_hall,
+                "star_count": member.star_count,
+                "attacks": self.parse_cw_attacks(member.attacks)
+            })
+        return home_members
+
+
+    async def verify_(self, playertag, token):
+        t = await self.client.verify_player_token(playertag, token)
+        return t
+
+
+def process_nested_data(data, depth=2, output_type='print', filename=None, encoding='utf-8'):
+    """
+    Обрабатывает вложенные структуры данных с заданными параметрами.
+    
+    Args:
+        data: данные для обработки (словарь, список или другой тип)
+        depth: максимальная глубина вывода (по умолчанию 2)
+        output_type: тип вывода ('print' - только печать, 'string' - сохранение в файл)
+        filename: имя файла для сохранения (обязателен при output_type='string')
+        encoding: кодировка файла (по умолчанию utf-8)
+    
+    Returns:
+        str: отформатированная строка при output_type='string', иначе None
+    
+    Raises:
+        ValueError: если указан неправильный тип вывода или отсутствует filename
+    """
+    # Проверка параметров
+    if output_type not in ['print', 'string']:
+        raise ValueError("output_type должен быть 'print' или 'string'")
+    
+    if output_type == 'string' and filename is None:
+        raise ValueError("При output_type='string' необходимо указать filename")
+    
+    # Внутренняя рекурсивная функция для форматирования
+    def _format_data(data, current_depth=0, indent=0, is_list_item=False):
+        result = ""
+        prefix = "  " * indent
+        if is_list_item:
+            prefix += "- "
+        
+        # Если достигли максимальной глубины
+        if current_depth >= depth:
+            if isinstance(data, dict):
+                result += f"{prefix}{{...}} (словарь с {len(data)} элементами)\n"
+            elif isinstance(data, list):
+                result += f"{prefix}[...] (список из {len(data)} элементов)\n"
+            else:
+                result += f"{prefix}{data}\n"
+            return result
+        
+        # Обработка словаря
+        if isinstance(data, dict):
+            for key, value in data.items():
+                result += f"{prefix}{key}: "
+                if isinstance(value, (dict, list)):
+                    result += "\n" + _format_data(value, current_depth + 1, indent + 1)
+                else:
+                    result += f"{value}\n"
+        
+        # Обработка списка
+        elif isinstance(data, list):
+            for i, item in enumerate(data):
+                if isinstance(item, (dict, list)):
+                    result += f"{prefix}Элемент {i + 1}:\n"
+                    result += _format_data(item, current_depth + 1, indent + 1, True)
+                else:
+                    result += f"{prefix}- {item}\n"
+        
+        # Обработка простых типов
+        else:
+            result += f"{prefix}{data}\n"
+        
+        return result
+    
+    # Форматируем данные
+    formatted_string = _format_data(data)
+    
+    # Обработка в зависимости от типа вывода
+    if output_type == 'print':
+        print(formatted_string, end='')
+        return None
+    else:  # output_type == 'string'
+        try:
+            with open(filename, 'w', encoding=encoding) as f:
+                f.write(formatted_string)
+            print(f"Данные успешно сохранены в файл: {filename}")
+            return formatted_string
+        except Exception as e:
+            print(f"Ошибка при сохранении файла: {e}")
+            return None
 
 async def main():
     getter = None
@@ -157,17 +434,22 @@ async def main():
         getter = DataGetter()
         await getter.login(APILogin, APIPassword)
 
-        #print(f"raids start: {coc.utils.get_raid_weekend_start()}")
-        #print(f"raids end: {coc.utils.get_raid_weekend_end()}")
-        #raidlog = await getter.get_raids_data(Clantag)
-        #print(raidlog)
-        #print(getter.check_is_player_attacked_in_raids(Playertag, raidlog))
-        #players = await getter.get_players_data(Clantag)
-        #print(getter.finalize_raidweekend(players, raidlog))
+
+        players = await getter.get_players_data(Clantag)
+        process_nested_data(players, depth=20, output_type="string", filename="players")
+
+        raidlog = await getter.get_raids_data(Clantag)
+        raid = getter.finalize_raidweekend(players, raidlog)
+        process_nested_data(raid, depth=20, output_type="string", filename="raid")
 
         cwl = await getter.get_cwl(Clantag)
-        print(cwl)
+        process_nested_data(cwl, depth=20, output_type="string", filename="cwl")
 
+        cw = await getter.get_cw("#2R8CP9G98")
+        process_nested_data(cw, depth=20, output_type="string", filename="cw")
+
+        #t = await getter.verify_("#YGGRO2YV2", "xddhhp3a") # Для проверки является ли пользователь главой клана
+        #print(t)
 
 
         print("Тестовый запуск успешен")
--- temp/storage/storage_creator.py
+++ temp/storage/storage_creator.py
@@ -1,2 +1,74 @@
-class StorageCreator():
-    ...
+import os
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
+
+
+class StorageCreator:
+    """
+    Класс для создания и проверки структуры файлового хранилища клана.
+    По умолчанию папка storage создаётся в той же директории, где находится этот файл.
+    """
+
+    def __init__(self, storage_root: Optional[str] = None):
+        """
+        :param storage_root: путь к корневой папке storage.
+            Если не указан, используется './storage' относительно расположения этого файла.
+        """
+        if storage_root is None:
+            # Папка, где лежит текущий файл
+            base_dir = Path(__file__).parent
+            self.storage_root = base_dir / 'storage'
+        else:
+            self.storage_root = Path(storage_root)
+
+    def ensure_clan_structure(self, clan_tag: str) -> None:
+        """
+        Создаёт всю необходимую структуру папок и timestamp-файлов для указанного клана.
+        Если какие-то элементы уже существуют, они не перезаписываются (кроме случая,
+        когда отсутствует timestamp-файл – он создаётся с текущим временем).
+
+        :param clan_tag: тег клана (например, '#2PQR89VGU')
+        """
+        # Папка клана
+        clan_dir = self.storage_root / f"clan_{clan_tag}"
+        clan_dir.mkdir(parents=True, exist_ok=True)
+
+        # Основной timestamp клана
+        self._ensure_timestamp_file(clan_dir / "main_timestamp.txt")
+
+        # Структура подпапок и соответствующие им timestamp-файлы
+        subdirs_config = {
+            "players/current_members": None,
+            "players/left_players": None,
+            "cw": "cw_timestamp.txt",
+            "cwl": "cwl_timestamp.txt",
+            "raids": "raids_timestamp.txt",
+            "clan_games": "cg_timestamp.txt",
+        }
+
+        for subdir, timestamp_file in subdirs_config.items():
+            dir_path = clan_dir / subdir
+            dir_path.mkdir(parents=True, exist_ok=True)
+
+            if timestamp_file:
+                self._ensure_timestamp_file(dir_path / timestamp_file)
+
+    def _ensure_timestamp_file(self, file_path: Path) -> None:
+        """
+        Вспомогательный метод: если файл не существует, создаёт его и записывает
+        текущую дату и время в формате 'last_updated: YYYY-MM-DD HH:MM:SS'.
+        """
+        if not file_path.exists():
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file_path.write_text(f'last_updated: {current_time}', encoding="utf-8")
+
+
+# Пример использования:
+if __name__ == "__main__":
+    # Без аргументов – storage появится рядом с этим файлом
+    creator = StorageCreator()
+    creator.ensure_clan_structure("#2PQR89VGU")
+
+    # Можно явно указать другой путь, например:
+    # creator = StorageCreator("/absolute/path/to/storage")
